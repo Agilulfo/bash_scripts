9b7a127bc4edaac37e0c698f2b850c806e9b8479
corsair <dinataleflavio@gmail.com>
2018-11-04 20:02:34 +0100

    Add additional customization to the keyboard mapping
    - use Fn keys as multimedia keys

--- model_m.sh
+++ model_m.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
+
 # Tweak my configuration to work better with my IBM model M Keyboard
+# meant to be used with a US international layout
+
 # Useful instructions on how to use xmodmap:
 # https://wiki.archlinux.org/index.php/xmodmap
 
 # Configure right control to work as a window key
+xmodmap -e "remove control = Control_R"
+xmodmap -e "add mod4 = Control_R"
+
+# Fn keys to media keys
+xmodmap -e "keycode  69 = XF86AudioLowerVolume XF86AudioLowerVolume F3 F3 F3 F3 XF86Switch_VT_3 F3 F3 XF86Switch_VT_3 F3 F3 F3 F3 XF86Switch_VT_3"
+xmodmap -e "keycode  70 = XF86AudioRaiseVolume XF86AudioRaiseVolume F4 F4 F4 F4 XF86Switch_VT_4 F4 F4 XF86Switch_VT_4 F4 F4 F4 F4 XF86Switch_VT_4"
+xmodmap -e "keycode  72 = XF86AudioPlay XF86AudioPlay F6 F6 F6 F6 XF86Switch_VT_6 F6 F6 XF86Switch_VT_6 F6 F6 F6 F6 XF86Switch_VT_6"
+xmodmap -e "keycode  73 = XF86AudioNext XF86AudioNext F7 F7 F7 F7 XF86Switch_VT_7 F7 F7 XF86Switch_VT_7 F7 F7 F7 F7 XF86Switch_VT_7"
+xmodmap -e "keycode  74 = XF86AudioPrev XF86AudioPrev F8 F8 F8 F8 XF86Switch_VT_8 F8 F8 XF86Switch_VT_8 F8 F8 F8 F8 XF86Switch_VT_8"
+
+# Alternative Graphics
 xmodmap -e "keycode  27 = r R r R egrave Egrave egrave"
-xmodmap -e "clear control"
-xmodmap -e "clear mod4"
-xmodmap -e "add control = Control_L"
-xmodmap -e "add mod4 = Super_L Super_R Hyper_L Control_R"
