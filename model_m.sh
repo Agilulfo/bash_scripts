180db32f3330e958b33c3ed8713863b2c8c7dcc8
corsair <dinataleflavio@gmail.com>
2019-10-05 14:18:10 +0200

    Enable italian vowels into model_m script

--- model_m.sh
+++ model_m.sh
@@ -18,4 +18,8 @@ xmodmap -e "keycode  73 = XF86AudioNext XF86AudioNext F7 F7 F7 F7 XF86Switch_VT_
 xmodmap -e "keycode  74 = XF86AudioPrev XF86AudioPrev F8 F8 F8 F8 XF86Switch_VT_8 F8 F8 XF86Switch_VT_8 F8 F8 F8 F8 XF86Switch_VT_8"
 
 # Alternative Graphics
-xmodmap -e "keycode  27 = r R r R egrave Egrave egrave"
+xmodmap -e "keycode  38 = a A a A agrave Agrave agrave"
+xmodmap -e "keycode  26 = e E e E egrave Egrave egrave"
+xmodmap -e "keycode  31 = i I i I igrave Igrave igrave"
+xmodmap -e "keycode  32 = o O o O ograve Ograve ograve"
+xmodmap -e "keycode  30 = u U u U ugrave Ugrave ugrave"
